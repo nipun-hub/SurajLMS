abc6b9cebc893397153b17e645d6204c9630a7d2
nipun-hub <82352923+nipun-hub@users.noreply.github.com>
2024-04-02 02:28:00 +0530

    Update conn.php

--- Dachbord/sql/conn.php
+++ Dachbord/sql/conn.php
@@ -1,7 +1,7 @@
  <?php
   //  database connection
-    // $conn=new mysqli("localhost","godadydulaxsuraj","JSo{5#rcV{kA","suraj_ict_lms");
-  $conn = new mysqli("127.0.0.1", "root", "", "ict_lms");
+    $conn=new mysqli("localhost","godadydulaxsuraj","JSo{5#rcV{kA","suraj_ict_lms");
+  // $conn = new mysqli("127.0.0.1", "root", "", "ict_lms");
 
   //  time zoon
   date_default_timezone_set("Asia/colombo");
