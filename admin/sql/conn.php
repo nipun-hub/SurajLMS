1a70c6ea0015574e0fb05fb29da3580aa8c39e6f
nipun-hub <82352923+nipun-hub@users.noreply.github.com>
2024-04-02 02:28:51 +0530

    Update conn.php

--- admin/sql/conn.php
+++ admin/sql/conn.php
@@ -1,7 +1,7 @@
  <?php
   //  database connection
-    // $conn=new mysqli("localhost","godadydulaxsuraj","JSo{5#rcV{kA","suraj_ict_lms");
-  $conn = new mysqli("127.0.0.1", "root", "", "ict_lms");
+    $conn=new mysqli("localhost","godadydulaxsuraj","JSo{5#rcV{kA","suraj_ict_lms");
+  // $conn = new mysqli("127.0.0.1", "root", "", "ict_lms");
 
 mysqli_set_charset($conn, "utf8mb4");
 
