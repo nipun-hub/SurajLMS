ef33204a93951b39efbe712cc14be845cda02fc5
nipun-hub <hemal@cypsolabs.com>
2024-06-26 00:26:11 +0530

    nav bar link profile

--- admin/include/navbar.php
+++ admin/include/navbar.php
@@ -1,96 +1,62 @@
 <!-- Page header starts -->
 <div class="page-header">
 
-<div class="toggle-sidebar" id="toggle-sidebar"><i class="bi bi-list"></i></div>
+    <div class="toggle-sidebar" id="toggle-sidebar"><i class="bi bi-list"></i></div>
 
-<!-- Breadcrumb start -->
-<ol class="breadcrumb d-md-flex d-none">
-    <li class="breadcrumb-item">
-        <i class="bi bi-house"></i>
-        <a href="index.html">Home</a>
-    </li>
-    <li class="breadcrumb-item breadcrumb-active" aria-current="page"><?php echo $_SESSION['active'] == 'dashbord' ? 'Dashbord' : ($_SESSION['active'] == 'dashbord' ? "Dashbord" : ($_SESSION['active'] == 'atendent' ? 'Atendent': ($_SESSION['active'] == 'notification' ? 'Notification' : ($_SESSION['active'] == 'lessonManage' ? 'Lesson Management' : ($_SESSION['active'] == 'addsnippet' ? 'Snippet Management' : ($_SESSION['active'] == 'peaperManagement' ? 'Peaper Management' : ($_SESSION['active'] == 'userManagement' ? 'User Management' : null)))) ))); ?></li>
-</ol>
-<!-- Breadcrumb end -->
-
-<!-- Header actions ccontainer start -->
-<div class="header-actions-container">
-
-    <!-- Search container start -->
-    <!-- <div class="search-container"> -->
-
-        <!-- Search input group start -->
-        <!-- <div class="input-group"> -->
-            <!-- <input type="text" class="form-control" placeholder="Search anything">
-            <button class="btn" type="button">
-                <i class="bi bi-search"></i>
-            </button>
-        </div> -->
-        <!-- Search input group end -->
-
-    <!-- </div> -->
-    <!-- Search container end -->
-
-    <!-- Leads start -->
-    <a href="notification.php" class="leads d-none d-xl-flex">
-        <div class="lead-details">You have <span class="count"> 0 </span> new leads </div>
-        <span class="lead-icon">
-            <i class="bi bi-bell-fill animate__animated animate__swing animate__infinite infinite"></i>
-            <b class="dot animate__animated animate__heartBeat animate__infinite"></b>
-        </span>
-    </a>
-    <!-- Leads end -->
-
-    <!-- Header actions start -->
-    <ul class="header-actions">
-        <!-- <li class="dropdown d-none d-md-block"> -->
-            <!-- <a href="#" id="countries" data-toggle="dropdown" aria-haspopup="true">
-                <img src="assets/images/flags/1x1/br.svg" class="flag-img" alt="Admin Panels" />
-            </a> -->
-            <!-- <a href="#" id="themeChange"> -->
-                <!-- <i class="bi bi-sun-fill" id="themeicon"></i> -->
-            <!-- </a> -->
-            <!-- <div class="dropdown-menu dropdown-menu-end mini" aria-labelledby="countries">
-                <div class="country-container">
-                    <a href="index.html">
-                        <img src="assets/images/flags/1x1/us.svg" alt="Clean Admin Dashboards" />
-                    </a>
-                    <a href="index.html">
-                        <img src="assets/images/flags/1x1/in.svg" alt="Google Dashboards" />
-                    </a>
-                    <a href="index.html">
-                        <img src="assets/images/flags/1x1/gb.svg" alt="AI Admin Dashboards" />
-                    </a>
-                    <a href="index.html">
-                        <img src="assets/images/flags/1x1/tr.svg" alt="Modern Dashboards" />
-                    </a>
-                    <a href="index.html">
-                        <img src="assets/images/flags/1x1/ca.svg" alt="Best Admin Dashboards" />
-                    </a>
-                </div>
-            </div> -->
-        <!-- </li> -->
-        <li class="dropdown">
-            <a href="#" id="userSettings" class="user-settings" data-toggle="dropdown" aria-haspopup="true">
-                <span class="user-name d-none d-md-block"><?php echo $UserName;?></span>
-                <span class="avatar">
-                    <img loading="lazy src="assets/images/user.png" alt="Admin Templates">
-                    <span class="status online"></span>
-                </span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="userSettings">
-                <div class="header-profile-actions">
-                    <a href="#">Profile</a>
-                    <a href="#">Settings</a>
-                    <a href="?logout">Logout</a>
-                </div>
-            </div>
+    <!-- Breadcrumb start -->
+    <ol class="breadcrumb d-md-flex d-none">
+        <li class="breadcrumb-item">
+            <i class="bi bi-house"></i>
+            <a href="index.html">Home</a>
         </li>
-    </ul>
-    <!-- Header actions end -->
+        <li class="breadcrumb-item breadcrumb-active" aria-current="page"><?php echo $_SESSION['active'] == 'dashbord' ? 'Dashbord' : ($_SESSION['active'] == 'dashbord' ? "Dashbord" : ($_SESSION['active'] == 'atendent' ? 'Atendent' : ($_SESSION['active'] == 'notification' ? 'Notification' : ($_SESSION['active'] == 'lessonManage' ? 'Lesson Management' : ($_SESSION['active'] == 'addsnippet' ? 'Snippet Management' : ($_SESSION['active'] == 'peaperManagement' ? 'Peaper Management' : ($_SESSION['active'] == 'userManagement' ? 'User Management' : ($_SESSION['active'] == 'profile' ? 'Profile' : ($_SESSION['active'] == 'contact' ? 'Contact Us' : ($_SESSION['active'] == 'massage' ? 'Massage' : null)))))))))); ?></li>
+    </ol>
+    <!-- Breadcrumb end -->
 
-</div>
-<!-- Header actions ccontainer end -->
+    <!-- Header actions ccontainer start -->
+    <div class="header-actions-container">
+
+        <!-- Leads start -->
+        <a href="notification.php" class="leads d-none d-xl-flex">
+            <div class="lead-details">You have <span class="count"> 0 </span> new notification </div>
+            <span class="lead-icon">
+                <i class="bi bi-bell-fill animate__animated animate__swing animate__infinite infinite"></i>
+                <b class="dot animate__animated animate__heartBeat animate__infinite"></b>
+            </span>
+        </a>
+        <!-- Leads end -->
+
+        <!-- Header actions start -->
+        <ul class="header-actions">
+            <?php
+            $sql = "SELECT image FROM adminuser WHERE AdminId = $UserId";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute();
+            $reusalt = $stmt->get_result();
+            $row = $reusalt->fetch_assoc();
+            $image = empty($row['image']) ? "../admin/assets/img/admin/admin.jpg" : "../admin/assets/img/admin/" . $row['image'];
+            ?>
+            <li class="dropdown">
+                <a href="#" id="userSettings" class="user-settings" data-toggle="dropdown" aria-haspopup="true">
+                    <span class="user-name d-none d-md-block"><?php echo $UserName; ?></span>
+                    <span class="avatar">
+                        <img loading="lazy" src="<?php echo $image ?>" alt="Admin">
+                        <span class="status online"></span>
+                    </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="userSettings">
+                    <div class="header-profile-actions">
+                        <a href="profile.php">Profile</a>
+                        <a href="profile.php">Settings</a>
+                        <a href="?logout">Logout</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <!-- Header actions end -->
+
+    </div>
+    <!-- Header actions ccontainer end -->
 
 </div>
 <!-- Page header ends -->
