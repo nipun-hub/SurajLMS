17f378e84a263e2117f56ef9b792ba3ad01857cb
nipun-hub <nipunthemal@gmail.com>
2025-01-07 02:11:45 +0530

    change landing page text bugs

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
+
 <head>
 
     <!--- basic page needs
     ================================================== -->
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    
+
     <meta name="description" content="Suraj S Kumara - A/L ICT - Online">
     <meta name="keywords" content="surajskumara , suraj s kumara , A/L ict , Online">
     <title>Suraj S Kumara</title>
@@ -37,7 +38,7 @@
     ================================================== -->
     <link rel="shortcut icon" href="assets/images/ict.ico">
     <link rel="icon" href="assets/images/ict.ico">
-    
+
 
 </head>
 
@@ -48,7 +49,7 @@
     <main class="s-home s-home--particles">
 
         <div id="particles-js" class="home-particles"></div>
-        
+
         <div class="gradient-overlay"></div>
 
         <div class="home-content">
@@ -59,7 +60,7 @@
                 <div class="home-slider-img" style="background-image: url(assets/images/slides/imge-3.jpg);"></div>
                 <div class="home-slider-img" style="background-image: url(assets/images/slides/imge-2.jpg);"></div>
             </div>
-    
+
             <div class="overlay"></div>
 
             <!-- <div class="home-logo">
@@ -74,8 +75,8 @@
                     <h3>Welcome to Surajskumara.lk</h3>
 
                     <h1>
-                    A/L ICT <br>
-                    With<br> Suraj S Kumara
+                        A/L ICT <br>
+                        With<br> Suraj S Kumara
                     </h1>
 
                     <p>
@@ -85,20 +86,20 @@
 
                     <div class="home-content__subscribe">
                         <!-- <form id="mc-form" class="group" novalidate="true"> -->
-                            <!-- <input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required=""> -->
-                            <!-- <input type="submit" name="subscribe" value="Notify Me"> -->
-                            <button onclick="window.location.href='Dachbord/';" class="btn btn--primary">Student Login</button>
-                            <!-- <button onclick="window.location.href='login/?login';" class="btn btn--primary">Student Login</button> -->
-                            <!-- <label for="mc-email" class="subscribe-message"></label> -->
+                        <!-- <input type="email" value="" name="EMAIL" class="email" id="mc-email" placeholder="Email Address" required=""> -->
+                        <!-- <input type="submit" name="subscribe" value="Notify Me"> -->
+                        <button onclick="window.location.href='Dachbord/';" class="btn btn--primary">Student Login</button>
+                        <!-- <button onclick="window.location.href='login/?login';" class="btn btn--primary">Student Login</button> -->
+                        <!-- <label for="mc-email" class="subscribe-message"></label> -->
                         <!-- </form> -->
                     </div>
-                </div>  <!-- end home-content__text -->
+                </div> <!-- end home-content__text -->
 
                 <div class="col-four home-content__counter">
-                    <h3>Teaches  - Suraj S Kumara</h3>
+                    <h3>Teach By - Suraj S Kumara</h3>
                     <img src="assets/images/suraj-imge-01.jpg" alt="suraj s kumara">
                     <!-- <div class="home-content__clock"> -->
-                        <!-- <div class="top">
+                    <!-- <div class="top">
                             <div class="time days">
                                 325
                                 <span>Days</span>
@@ -117,31 +118,31 @@
                             <span>S</span>
                         </div> -->
                     <!-- </div>  -->
-                </div>  
+                </div>
 
-            </div>  <!-- end home-content__main -->
+            </div> <!-- end home-content__main -->
 
             <ul class="home-social">
                 <li>
-                <a href="https://www.facebook.com/surajsumudukumara.suraj?mibextid=LQQJ4d" style="color:#0866FF;"><i class="fab fa-facebook-f" aria-hidden="true"></i><span>Facebook</span></a>
+                    <a href="https://www.facebook.com/surajsumudukumara.suraj?mibextid=LQQJ4d" style="color:#0866FF;"><i class="fab fa-facebook-f" aria-hidden="true"></i><span>Facebook</span></a>
                 </li>
                 <li>
-                <a href="https://youtube.com/@surajskumaraict5175?si=hjLjC6DkyWAKozWU" style="color:#ff0000;"><i class="fab fa-youtube" aria-hidden="true"></i><span>Youtube</span></a>
+                    <a href="https://youtube.com/@surajskumaraict5175?si=hjLjC6DkyWAKozWU" style="color:#ff0000;"><i class="fab fa-youtube" aria-hidden="true"></i><span>Youtube</span></a>
                 </li>
                 <li>
-                <a href="https://instagram.com/suraj_s_kumara_ict_official?igshid=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr" style="color:#DD2A7B ;"><i class="fab fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
+                    <a href="https://instagram.com/suraj_s_kumara_ict_official?igshid=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr" style="color:#DD2A7B ;"><i class="fab fa-instagram" aria-hidden="true"></i><span>Instagram</span></a>
                 </li>
                 <li>
-                <a href="https://whatsapp.com/channel/0029Va5JDCZ77qVMKEm9Ji2y" style="color:#25D366;"><i class="fab fa-whatsapp" aria-hidden="true"></i><span>Whatsapp</span></a>
+                    <a href="https://whatsapp.com/channel/0029Va5JDCZ77qVMKEm9Ji2y" style="color:#25D366;"><i class="fab fa-whatsapp" aria-hidden="true"></i><span>Whatsapp</span></a>
                 </li>
                 <li>
-                <a href="#" style="color: #25A3E2;"><i class="fab fa-telegram" aria-hidden="true"></i><span>Telegram</span></a>
+                    <a href="#" style="color: #25A3E2;"><i class="fab fa-telegram" aria-hidden="true"></i><span>Telegram</span></a>
                 </li>
             </ul> <!-- end home-social -->
 
             <div class="row home-copyright">
-                <span>© Copyright Website <a href="https://surajskumara.lk">surajskumara.lk</a>. All rights reserved.</span> 
-                <span>Powered by <a href="#">Genie-X Technology</a></span>
+                <span>&copy; Copyright Website <a href="https://surajskumara.lk">surajskumara.lk</a>. 2023 All rights reserved.</span>
+                <span>Powered by <a href="#">Elight Technologies</a></span>
             </div> <!-- end home-copyright -->
 
 
@@ -165,11 +166,11 @@
             <div class="col-seven tab-full info-main">
                 <h1>We Teach Advanced Level ICT</h1>
                 <p>
-                උසස්පෙළ තොරතුරු සහ සන්නිවේදන තාක්ෂණ ගුරුවරයෙකු විසින් මෙහෙයවනු ලබයි.
+                    උසස්පෙළ තොරතුරු සහ සන්නිවේදන තාක්ෂණ ගුරුවරයෙකු විසින් මෙහෙයවනු ලබයි.
                 </p>
 
                 <p>
-                අපගේ ICT පන්තිගේ සියලුම පන්ති විස්තර ලබා ගැනීමට , පංති සදහා සහභාගී වීමට ,  රෙකෝඩින් ලබා ගැනිමට , විභාග සදහා සම්බන්ද වීමට හා Free class සදහා සම්බන්ද වීමට Register වන්න'
+                    අපගේ ICT පන්තිගේ සියලුම පන්ති විස්තර ලබා ගැනීමට , පංති සදහා සහභාගී වීමට , රෙකෝඩින් ලබා ගැනිමට , විභාග සදහා සම්බන්ද වීමට හා Free class සදහා සම්බන්ද වීමට Register වන්න'
                 </p>
             </div>
 
@@ -186,46 +187,45 @@
 
                 <div class="info-block">
                     <h3>Our Classes</h3>
-                    
+
                     <p class="info-address">
                         Susipwan - Gampaga<br>
-                        Sasip - Nugegoda<br>
                         Wins - Weyangoda<br>
-                        Nuwana - Kegalla<br>
+                        Siyowin - Kegalla<br>
                         Online
                     </p>
                 </div>
 
                 <div class="info-block">
                     <h3>Find Us On</h3>
-                    
+
                     <ul class="info-social">
                         <li>
                             <a href="https://www.facebook.com/surajsumudukumara.suraj?mibextid=LQQJ4d" style="color:#0866FF;"><i class="fab fa-facebook" aria-hidden="true"></i>
-                            <span>Facebook</span></a>
+                                <span>Facebook</span></a>
                         </li>
                         <li>
                             <a href="https://youtube.com/@surajskumaraict5175?si=hjLjC6DkyWAKozWU" style="color:#ff0000;"><i class="fab fa-youtube" aria-hidden="true"></i>
-                            <span>Youtube</span></a>
+                                <span>Youtube</span></a>
                         </li>
                         <li>
                             <a href="https://instagram.com/suraj_s_kumara_ict_official?igshid=NGVhN2U2NjQ0Yg%3D%3D&utm_source=qr" style="color:#DD2A7B ;"><i class="fab fa-instagram" aria-hidden="true"></i>
-                            <span>Instagram</span></a>
+                                <span>Instagram</span></a>
                         </li>
                         <li>
                             <a href="https://whatsapp.com/channel/0029Va5JDCZ77qVMKEm9Ji2y" style="color:#25D366;"><i class="fab fa-whatsapp" aria-hidden="true"></i>
-                            <span>Whatsapp</span></a>
+                                <span>Whatsapp</span></a>
                         </li>
                         <li>
                             <a href="#0" style="color: #25A3E2;"><i class="fab fa-telegram" aria-hidden="true"></i>
-                            <span>Teligram</span></a>
+                                <span>Teligram</span></a>
                         </li>
                     </ul>
                 </div>
-                
-            </div>  <!-- end info contact -->
 
-        </div>  <!-- end info wrapper -->
+            </div> <!-- end info contact -->
+
+        </div> <!-- end info wrapper -->
 
     </div> <!-- end s-info -->
 
@@ -244,19 +244,20 @@
         </div>
     </div> -->
     <div id="loading-wrapper">
-		<div class="spinner">
+        <div class="spinner">
             <div class="line1"></div>
-			<div class="line2"></div>
-			<div class="line3"></div>
-			<div class="line4"></div>
-			<div class="line5"></div>
-			<div class="line6"></div>
+            <div class="line2"></div>
+            <div class="line3"></div>
+            <div class="line4"></div>
+            <div class="line5"></div>
+            <div class="line6"></div>
         </div>
-	</div>
+    </div>
 
     <!-- apecial animated link  -->
-    <?php //include_once('include/animated-canvers.php'); ?>
-    
+    <?php //include_once('include/animated-canvers.php'); 
+    ?>
+
     <!-- Messenger Chat plugin Code start -->
     <div id="fb-root"></div>
 
@@ -264,30 +265,31 @@
     <div id="fb-customer-chat" class="fb-customerchat"></div>
 
     <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "113750700465930");
-      chatbox.setAttribute("attribution", "biz_inbox");
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "113750700465930");
+        chatbox.setAttribute("attribution", "biz_inbox");
     </script>
 
     <!-- Your SDK code -->
     <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v18.0'
-        });
-      };
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml: true,
+                version: 'v18.0'
+            });
+        };
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-      </script>
-      
-      <!-- Your Chat plugin code end -->
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+
+    <!-- Your Chat plugin code end -->
 
     <!-- Java Script
     ================================================== -->
